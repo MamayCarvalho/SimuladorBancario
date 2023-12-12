4ff503de038ee4d77cdc21cba12407b09b50df94
MamayCarvalho <115594676+MamayCarvalho@users.noreply.github.com>
2023-12-11 22:37:31 -0300

    Update ContaPJ.java

--- ContaPJ.java
+++ ContaPJ.java
@@ -1,5 +1,31 @@
 package Banco;
 
-public class ContaPJ {
+public abstract class ContaPJ extends Conta {
+	
+	protected double emprestimo;
+	protected final double limiteEmprestimo = 2500.00;
+	
+	public ContaPJ(String titular, double limiteEmprestimo) {
+		super(titular, "PJ");
+	}
+	
+	public double getEmprestimo() {
+		return emprestimo;
+	}
 
+	public void setEmprestimo(double emprestimo) {
+		this.emprestimo = emprestimo;
+	}
+	
+	private double getLimiteEmprestimo() {
+		return limiteEmprestimo;
+	}
+	
+	public void emprestimo(double dinheiro) {
+		if(dinheiro > getLimiteEmprestimo()) {
+			System.out.println("Seu limite de emprestimo é de R$" + getLimiteEmprestimo());
+		} else {
+			saque(dinheiro);
+		}
+	}
 }
